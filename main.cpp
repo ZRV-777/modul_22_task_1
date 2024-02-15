8239935ed9b6d25d8e4e46e17124f7a83ce10d07
Roman Zaryuta <zel_smer4@bk.ru>
2024-02-15 19:27:24 +0300

    modify code

--- main.cpp
+++ main.cpp
@@ -5,17 +5,19 @@
 
 using namespace std;
 
-void insert_in_map(map<string, string> &myMap, string phone_number, string last_name)
+void insert_phone_to_lastname(map<string, string> &myMap, string phone_number, string last_name)
 {
-    cout << "Input phone number: ";
-    cin >> phone_number;
-    cout << "Input last name: ";
-    cin >> last_name;
     myMap.insert(make_pair(phone_number, last_name));
 }
 
-void find_last_name(map<string, string> &myMap, string phone_number)
+void insert_lastname_to_phone(map<string, vector<string>> &myMap, string phone_number, string last_name)
 {
+    myMap[last_name].push_back(phone_number);
+}
+
+void find_last_name(map<string, string> &myMap)
+{
+    string phone_number;
     cout << "Input phone number: ";
     cin >> phone_number;
     map<string, string>::iterator it_phone = myMap.find(phone_number);
@@ -25,63 +27,64 @@ void find_last_name(map<string, string> &myMap, string phone_number)
     }
 }
 
-void find_phone_number(map<string, string> &myMap, string last_name)
+void find_phone_numer(map<string, vector<string>> &myMap)
 {
+    string last_name;
     cout << "Input last name: ";
     cin >> last_name;
 
-    string target = last_name;
-    vector<string> counter;
-
-    for (auto it = myMap.begin(); it != myMap.end(); it++)
+    map<string, vector<string>>::iterator it_last_name = myMap.find(last_name);
+    if (it_last_name != myMap.end())
     {
-        if (it->second == target)
+        cout << "Phone numbers: ";
+        for(int i = 0; i < it_last_name->second.size(); ++i)
         {
-            counter.push_back(it->first);
+            cout << it_last_name->second[i] << " ";
         }
-    }
-
-    for (const auto &counter: counter)
-    {
-        cout << "Phone number: ";
-        cout << counter << " ";
     }
 }
 
 int main()
 {
-    map<string, string> phone_book;
+    map<string, string> phone_to_lastname;
+    map<string, vector<string>> lastname_to_phone;
     string phone_number;
     string last_name;
     string command;
 
-    cout << "Input command (add, name, phone): ";
+    cout << "Input command (add, Lname, phone): ";
     cin >> command;
 
     while (command != "exit")
     {
         if (command == "add")
         {
-            insert_in_map(phone_book, phone_number, last_name);
-            cout << endl << "Input command (add, name, phone or exit): ";
+            cout << "Enter phone number: ";
+            cin >> phone_number;
+            cout << "Enter last name: ";
+            cin >> last_name;
+
+            insert_phone_to_lastname(phone_to_lastname, phone_number, last_name);
+            insert_lastname_to_phone(lastname_to_phone, phone_number, last_name);
+            cout << endl << "Input command (add, Lname, phone or exit): ";
             cin >> command;
         }
-        else if (command == "name")
+        else if (command == "Lname")
         {
-            find_last_name(phone_book, phone_number);
-            cout << endl << "Input command (add, name, phone): ";
+            find_last_name(phone_to_lastname);
+            cout << endl << "Input command (add, Lname, phone): ";
             cin >> command;
         }
         else if (command == "phone")
         {
-            find_phone_number(phone_book, last_name);
-            cout << endl << "Input command (add, name, phone): ";
+            find_phone_numer(lastname_to_phone);
+            cout << endl << "Input command (add, Lname, phone): ";
             cin >> command;
         }
         else
         {
             cerr << "Uknown command";
-            cout << endl << "Input command (add, name, phone): ";
+            cout << endl << "Input command (add, Lname, phone): ";
             cin >> command;
         }
     }
